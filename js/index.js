c0e94b27dae7c1e2d0d77043d30d850bab9429e4
MM1377 <77946174+MM1377@users.noreply.github.com>
2023-03-09 16:05:19 -0500

    Backend Stuff

--- js/index.js
+++ js/index.js
@@ -1,6 +1,171 @@
+//include jQuery
+var jQueryScript = document.createElement('script');  
+jQueryScript.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js');
+document.head.appendChild(jQueryScript);
 
+
+function login(e) {
+    //delete cookie
+    console.log("login function started ooooooooooooooooooooooooooooooooooooooooooooooooooo");
+    e.preventDefault();
+
+    var vEmail = document.getElementById("email").value;
+    var vPassword = document.getElementById("pwd").value;
+    var vLoggedIn = false;
+     
+    $(document).ready(function(){
+        $.ajax({
+            url: 'http://localhost/rideratersback/login2.php',
+            dataType: 'json',
+            async: 'false',
+            type: 'post',
+            data: {username: vEmail, password: vPassword},
+            success: function(response) {            
+                //alert("Alert 1: " + JSON.stringify(response));
+                console.log("log 1: " + JSON.stringify(response));
+                // Handle the response from the PHP script
+                if(response.success == "true")
+                { 
+                    //create cookie with uid, psw, logged in = true
+                    //transfer to other page 
+                    window.location.href = "home.html";
+                    console.log("log 2: " + JSON.stringify(response));
+                    //alert("Alert 2: " + 'success');
+                } else {                
+                    console.log("log 3: " + JSON.stringify(response));
+                    alert('Invalid User ID or Password');
+                    window.location.href = "login.html";
+                };
+            },
+            error: function(xhr, status, error) {
+                console.log(xhr.responseText);
+                alert('Error: ' + xhr.responseText);                
+                window.location.href = "login.html";
+            }
+        }); 
+    });
+    //alert("test end");
+}
+
+function signup(e) {
+    //delete cookie
+    console.log("signup function started ooooooooooooooooooooooooooooooooooooooooooooooooooo");
+    e.preventDefault();
+
+    var vEmail = document.getElementById("email").value;
+    var vPassword = document.getElementById("pwd").value;
+    //alert(vEmail + '=' + vPassword);
+
+    $(document).ready(function(){
+
+        $.ajax({
+            url: 'http://localhost/rideratersback/signup2.php',
+            dataType: 'json',
+            async: 'false',
+            type: 'post',
+            data: {username: vEmail, password: vPassword},
+            success: function(response) {            
+                //alert("Alert 1: " + JSON.stringify(response));
+                console.log("log 1: " + JSON.stringify(response));
+                // Handle the response from the PHP script
+                if(response.success == "true")
+                {
+                    //create cookie with uid, psw, logged in = true
+                    //transfer to other page 
+                    window.location.href = "login.html";
+
+                    console.log("log 2: " + JSON.stringify(response));
+                // alert("Alert 2: " + 'success');
+                } else {                
+                    console.log("log 3: " + JSON.stringify(response));
+                    alert('System error, trying to figure this out');
+                };
+            },
+            error: function(xhr, status, error) {
+                console.log(xhr.responseText);
+                alert('Error: ' + xhr.responseText);
+            },
+            complete: function (response){
+                console.log("Ajax completed");
+                //alert('Ajax completed');
+
+                }
+        });
+    })
+   // alert("test end");
+}
+
+
+function resetPassword(e) {
+    //delete cookie
+    console.log("login function started ooooooooooooooooooooooooooooooooooooooooooooooooooo");
+
+    var vEmail = document.getElementById("email").value;
+    var vPassword = document.getElementById("pwd").value;
+    var vNewPassword1 = document.getElementById("newpwd1").value;
+    var vNewPassword2 = document.getElementById("newpwd2").value;
+     
+    if (vNewPassword1 != vNewPassword2) {
+        console.log("passwords must match");
+        alert("passwords must match");                
+        window.location.replace = "resetpass.html";
+        exit();
+    }
+
+    if (vPassword == vNewPassword2) {
+        console.log("password must be new");
+        alert("password must be new");                
+        window.location.replace = "resetpass.html";
+        exit();
+    }
+
+    $(document).ready(function(){
+        e.preventDefault();
+        return $.ajax({
+            url: 'http://localhost/rideratersback/resetpass.php',
+            dataType: 'json',
+            async: 'false',
+            type: 'post',
+            data: {username: vEmail, password: vPassword, passwordNew: vNewPassword1},
+            success: function(response) {            
+                //alert("Alert 1: " + JSON.stringify(response));
+                console.log("log 1: " + JSON.stringify(response));
+                // Handle the response from the PHP script
+                if(response.success == "true")
+                { 
+                    //create cookie with uid, psw, logged in = true
+                    //transfer to other page 
+                   // window.location.href = "home.html";
+                    console.log("log 2: " + JSON.stringify(response));
+                    window.location.href = "login.html";
+                    //alert("Alert 2: " + 'success');
+                } else {                
+                    console.log("log 3: " + JSON.stringify(response));
+                    alert('Invalid Entry');
+                    window.location.replace = "resetpass.html";
+                };
+            },
+            error: function(xhr, status, error) {
+                console.log(xhr.responseText);
+                alert('Error: ' + xhr.responseText);                
+                window.location.replace = "resetpass.html";
+            }
+        }); 
+    });
+    //alert("test end");
+}
+
+resetPassword().then( response =>
+    
+   console.log("ajax complete")
+    );
+
+
+
+
+ 
 // fetch data from json file and store in variables for use in other functions and pages 
-fetch('/data/data.json')
+fetch('/rideraters/data/data.json')
   .then(response => response.json())
   .then(data => {
     console.log(data);
